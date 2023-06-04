7df6fe0c60dc978c56ca9666b4c8486bed4c4783
dochien0204 <doxuanchienh02042002@gmail.com>
2023-06-04 19:39:33 +0700

    nested layout with react-router

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -3,19 +3,26 @@ import { createBrowserRouter, createRoutesFromElements,  Route, RouterProvider }
 import './App.css';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
+import RootLayout from './pages/RootLayout';
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path='/' element={<HomePage/>} />
-    <Route path='/login' element={<Login />} />
-  </Route>
-)
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage/>} />
+//     <Route path='/login' element={<Login />} />
+//   </Route>
+// )
 
-const router = createBrowserRouter(routeDefinitions)
-// const router = createBrowserRouter([
-//   {path: '/', element: <HomePage />},
-//   {path: '/login', element: <Login />}
-// ])
+// const router = createBrowserRouter(routeDefinitions)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {path: '/', element: <HomePage />},
+      {path: '/login', element: <Login />}
+    ],
+  },
+])
 
 function App() {
   return <RouterProvider router={router}/>
--- react_router/src/pages/HomePage.js
+++ react_router/src/pages/HomePage.js
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom"
+
 const HomePage = () => {
-    return <div>Home Page</div>
+    return <>
+    <div>Home Page</div>
+    <Link to="/login">Go to Login page</Link>
+    </>
 }
 
 export default HomePage
