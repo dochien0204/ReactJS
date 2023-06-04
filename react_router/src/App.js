23daebc19fc3020e86f9b313f0e9c8fecaf508c1
dochien0204 <doxuanchienh02042002@gmail.com>
2023-06-04 18:56:06 +0700

    define router

---
+++ react_router/src/App.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './App.css';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />},
+])
+
+function App() {
+  return <RouterProvider router={router}/>
+}
+
+export default App;
