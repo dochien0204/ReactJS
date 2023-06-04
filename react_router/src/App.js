b589164b41f7d481427e143591832d974b6c17cd
dochien0204 <doxuanchienh02042002@gmail.com>
2023-06-04 18:58:13 +0700

    Add second path

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -2,9 +2,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {path: '/', element: <HomePage />},
+  {path: '/login', element: <Login />}
 ])
 
 function App() {
