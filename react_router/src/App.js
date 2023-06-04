a8cf089a69aad2ac4213b221cc991f243953d29e
dochien0204 <doxuanchienh02042002@gmail.com>
2023-06-04 19:07:15 +0700

    an alternative router definition

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,13 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements,  Route, RouterProvider } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 
-const router = createBrowserRouter([
-  {path: '/', element: <HomePage />},
-  {path: '/login', element: <Login />}
-])
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path='/' element={<HomePage/>} />
+    <Route path='/login' element={<Login />} />
+  </Route>
+)
+
+const router = createBrowserRouter(routeDefinitions)
+// const router = createBrowserRouter([
+//   {path: '/', element: <HomePage />},
+//   {path: '/login', element: <Login />}
+// ])
 
 function App() {
   return <RouterProvider router={router}/>
